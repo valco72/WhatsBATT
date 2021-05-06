865f9eaa9c17ad9083e08401ca0f20119d1a0692
Batuhan <62402999+valco72@users.noreply.github.com>
2021-05-06 14:48:57 +0300

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -14,31 +14,50 @@ const Lang = Language.getString('instagram')
 const Tlang = Language.getString('tiktok')
 
 if (cn.WORKTYPE == 'private') {
-
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+	
+    const userName = match[1]
+    
+    if (!userName) return await message.sendMessage(errorMessage('Lütfen kullanıcı adını doğru girdiğinizden emin olun.'))
+	
+    await message.sendMessage(infoMessage('Profil bilgileri yükleniyor.'))
 
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
+    await axios
+      .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
+      .then(async (response) => {
+        const {
+          profile_hd,
+          username,
+          bio,
+          followers,
+          following,
+          full_name,
+          is_private,
+        } = response.data.result
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
+        const profileBuffer = await axios.get(profile_pic_url_hd, {
+        const profileBuffer = await axios.get(profile_hd, {
+          responseType: 'arraybuffer',
+        })
 
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
+        const msg = `
+        *${Lang.NAME}*: ${full_name}
+        *${Lang.USERNAME}*: ${username}
+        *${Lang.BIO}*: ${bio}
+        *${Lang.FOLLOWERS}*: ${followers}
+        *${Lang.FOLLOWS}*: ${following}
+        *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
 
-    }));
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(userName + 'Kullanıcının bilgilerinin tam olduğundan emin olun.')),
+      )
+  },
+)
 
     /*
     Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: Tlang.TİKTOK }, async (message, match) => {
@@ -73,54 +92,93 @@ if (cn.WORKTYPE == 'private') {
 }
 else if (cn.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
-
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
-
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
-
-    }));
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc: Lang.DESC, dontAddCommandList: true }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+    const userName = match[1]
+    
+    if (!userName) return await message.sendMessage(errorMessage('Lütfen kullanıcı adını doğru girdiğinizden emin olun.'))
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=ab9942f95c09ca89`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
+    await message.sendMessage(infoMessage('Profil bilgileri yükleniyor.'))
 
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
+    await axios
+      .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
+      .then(async (response) => {
+        const {
+          profile_hd,
+          username,
+          bio,
+          followers,
+          following,
+          full_name,
+          is_private,
+        } = response.data.result
 
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
+        const profileBuffer = await axios.get(profile_pic_url_hd, {
+        const profileBuffer = await axios.get(profile_hd, {
+          responseType: 'arraybuffer',
+        })
 
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
+        const msg = `
+        *${Lang.NAME}*: ${full_name}
+        *${Lang.USERNAME}*: ${username}
+        *${Lang.BIO}*: ${bio}
+        *${Lang.FOLLOWERS}*: ${followers}
+        *${Lang.FOLLOWS}*: ${following}
+        *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
 
-    }));
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(userName + 'Kullanıcının bilgilerinin tam olduğundan emin olun.')),
+      )
+  },
+);
+	
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
+	
+    const userName = match[1]
+    
+    if (!userName) return await message.sendMessage(errorMessage('Lütfen kullanıcı adını doğru girdiğinizden emin olun.'))
+	
+    await message.sendMessage(infoMessage('Profil bilgileri yükleniyor.'))
+
+    await axios
+      .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
+      .then(async (response) => {
+        const {
+          profile_hd,
+          username,
+          bio,
+          followers,
+          following,
+          full_name,
+          is_private,
+        } = response.data.result
+
+        const profileBuffer = await axios.get(profile_pic_url_hd, {
+        const profileBuffer = await axios.get(profile_hd, {
+          responseType: 'arraybuffer',
+        })
+
+        const msg = `
+        *${Lang.NAME}*: ${full_name}
+        *${Lang.USERNAME}*: ${username}
+        *${Lang.BIO}*: ${bio}
+        *${Lang.FOLLOWERS}*: ${followers}
+        *${Lang.FOLLOWS}*: ${following}
+        *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(userName + 'Kullanıcının bilgilerinin tam olduğundan emin olun.')),
+      )
+  },
+);
     /*
     Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: false, desc: Tlang.TİKTOK }, async (message, match) => {
 
